b1efb364501c5b6cfd9ebe10c817611fd4f3ea5a
Orca <orca.wannakham@gmail.com>
2026-09-07 10:40:26 +0000

    add rgb nonsens

--- build/main.lua
+++ build/main.lua
@@ -1,4 +1,16 @@
+function love.load()
+    t = 0
+end
+
+function love.update(dt)
+    t = t + dt
+end
+
 function love.draw()
-   love.graphics.print("passsss",20,20)
-   love.graphics.circle("fill", 80, 80, 34)
+    local r = (math.sin(t) + 1) / 2
+    local g = (math.sin(t + 2.094) + 1) / 2
+    local b = (math.sin(t + 4.189) + 1) / 2
+
+    love.graphics.setColor(r, g, b)
+    love.graphics.rectangle("fill", 0, 0, love.graphics.getWidth(), love.graphics.getHeight())
 end
